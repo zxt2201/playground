18d91f117d0ba04ec7f11e0de167cdc75947d9a6
Steven Shaw <steshaw@gmail.com>
2010-01-09 22:57:06 +1000

    .

--- resty/src/main/scala/steshaw/resty/resource/ScalaResource.scala
+++ resty/src/main/scala/steshaw/resty/resource/ScalaResource.scala
@@ -1,9 +1,12 @@
 package steshaw.resty.resource
 
+import steshaw.resty.User
+import java.util.Date
 import javax.ws.rs.{GET, Produces, Path, PathParam}
 
-@Path("scala")
+@Path("/scala")
 class ScalaResource {
+
     @GET
     @Produces(Array("text/plain"))
     def helloScala = "RESTful hello from Scala!\n";
@@ -13,9 +16,25 @@ class ScalaResource {
     def java = "Hello. " + steshaw.resty.resource.JavaResource.message + "\n"
 
     @GET
-    @Path("user/{id}")
-    @Produces(Array("text/plain"))
-    def blah(@PathParam("id") id:String) = "Hello " + id + ".\n"
+    @Path("/user/{id}")
+    @Produces(Array("application/json", "application/xml"))
+    def blah(@PathParam("id") id:String) = User(id, "Steven Shaw", "steshaw@gmail.com")
+
+    val name = "Steve"
+
+    //
+    // See https://jersey.dev.java.net/source/browse/jersey/trunk/jersey/samples/scala-helloworld-webapp/src/main/scala/com/sun/jersey/samples/helloworld/resources/MarkupResource.scala?annotate=2305
+    //
+    @GET
+    @Path("/user/steve")
+    @Produces(Array("text/html"))
+    def get =
+      <html>
+        <body>
+          <h1>Hello {name}</h1>
+          <p>Generated at {new Date()}</p>
+        </body>
+      </html>
 }
 
 object ScalaResource {
--- resty/test.sh
+++ resty/test.sh
@@ -9,7 +9,13 @@ curl http://localhost:8888/resty/java/scala
 curl http://localhost:8888/resty/scala
 curl http://localhost:8888/resty/scala/
 curl http://localhost:8888/resty/scala/java
+curl http://localhost:8888/resty/scala/user/steve/
+set +x
 curl http://localhost:8888/resty/scala/user/1
+echo
 curl http://localhost:8888/resty/scala/user/2
-curl http://localhost:8888/resty/scala/user/1/
-curl http://localhost:8888/resty/scala/user/2/
+echo
+curl -H "Accept: application/xml" http://localhost:8888/resty/scala/user/1
+echo
+curl -H "Accept: application/xml" http://localhost:8888/resty/scala/user/2
+echo
