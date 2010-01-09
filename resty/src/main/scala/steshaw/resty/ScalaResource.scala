24f8cfba7c1b778fde9677739e5907fa08fbee71
Steven Shaw <steshaw@gmail.com>
2010-01-09 16:50:01 +1000

    .

---
+++ resty/src/main/scala/steshaw/resty/ScalaResource.scala
@@ -0,0 +1,14 @@
+package steshaw.resty
+
+import javax.ws.rs.GET
+import javax.ws.rs.Produces
+import javax.ws.rs.Path
+
+@Path("scala")
+class ScalaResource {
+
+    @GET
+    @Produces(Array("text/plain"))
+    def helloScala(): String = "RESTful hello from Scala!\n";
+
+}
